65e324d6181a07801b091c942637756ed9d91b10
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2024-01-26 08:07:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/pamrj/Nysamusic",
+    "https://github.com/Badnam143/Badnamanonx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
